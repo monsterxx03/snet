b26e686688bdf31af879694cf22714b2d151f285
monsterxx03 <xyj.asmy@gmail.com>
2019-07-03 21:17:04 +0800

    Close upstream connection explictly

--- server.go
+++ server.go
@@ -65,6 +65,7 @@ func (s *Server) handle(conn *net.TCPConn) error {
 	if err != nil {
 		return err
 	}
+	defer remoteConn.Close()
 	// if intercept is enabled, use i to replace conn
 	// i := proxy.NewIntercept(conn, dstHost, dstPort, l)
 	go s.proxy.Pipe(conn, remoteConn)
